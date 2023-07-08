a014214ea73dfca9215a481a0d021b2b1a2b8d04
VidSovic <61434487+VidSovic@users.noreply.github.com>
2023-07-08 15:46:40 +0200

    Update testing9.js

--- testing9.js
+++ testing9.js
@@ -1,12 +1,10 @@
 // Define the IP and port of your server
 const serverIP = '195.201.121.249';
 const serverPort = '8888';
-
 // Build the URL
 const serverUrl = `http://${serverIP}:${serverPort}`;
 
-// Using the Fetch API to send a GET request
-fetch(serverUrl)
-  .then(response => response.text())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
+// Create a new image object
+var img = new Image();
+// Set the src of the image to your server
+img.src = serverUrl;
